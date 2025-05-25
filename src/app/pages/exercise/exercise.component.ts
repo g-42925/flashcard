b741ac614f7239620fc56d7e8cdd89add03c7821
g-42925 <ecipersia@gmail.com>
2025-05-25 07:07:35 +0700

    c-2505250707

--- src/app/pages/exercise/exercise.component.ts
+++ src/app/pages/exercise/exercise.component.ts
@@ -175,7 +175,7 @@ export class ExerciseComponent implements OnInit {
     this.http.delete(`${this.source}/${id}`,{headers,withCredentials:true}).subscribe(r => {
       var index = this.words.findIndex(w => w.id === id)
 
-      this.updateValue = this.words[index - 1]
+      this.updateValue = this.words[index + 1]
 
       this.words = this.words.filter(w => w.id != id)
     });
