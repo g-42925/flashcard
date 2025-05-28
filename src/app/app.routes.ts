df5666806f2f74ac30b2a350d8a119665fbcf44a
g-42925 <ecipersia@gmail.com>
2025-05-28 13:46:15 +0700

    c-2505281346

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { ExerciseComponent } from './pages/exercise/exercise.component';
 import { ResultComponent } from './pages/result/result.component';
-import { SearchComponent } from './pages/search/search.component'
 
 export const routes: Routes = [
-  {path:'search',component:SearchComponent},
   {path:'exercise',component:ExerciseComponent},
   {path:'result',component:ResultComponent}
 ];
