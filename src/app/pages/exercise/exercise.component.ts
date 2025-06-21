5b73e8c144d3fcd1d9de382f56a96051b2635603
g-42925 <ecipersia@gmail.com>
2025-06-21 14:28:30 +0700

    c25621228

--- src/app/pages/exercise/exercise.component.ts
+++ src/app/pages/exercise/exercise.component.ts
@@ -396,7 +396,7 @@ export class ExerciseComponent implements OnInit {
 
     if(!filter2) {
       this.forgottenWords = [
-        this.words[idx],
+        filter1,
         ...this.forgottenWords,
       ] 
     }
